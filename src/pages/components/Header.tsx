50dd4e3f593b1f353f75a2293d6f51c8acfe1bbc
Alex Paris <alex_paris28@hotmail.com>
2022-12-06 14:16:03 -0300

    chore: fix

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,14 +1,11 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export function Header() {
   const test = "teste secundarionrrrrr"
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <meta name="description" content={test} />
-      </Head>
-    </div>
+    <Head>
+      <meta name="description" content={test} />
+    </Head>
   )
 }
